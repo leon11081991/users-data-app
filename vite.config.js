0b05c75d409ad7ddce985855496beb6adcc4822a
Leon Chou <iamyiii0328@gmail.com>
2023-09-18 14:20:08 +0800

    setup- vue, tailwind finished

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            // 避免混淆，創建 Vue 的檔案名稱改為 main.js
+            input: ["resources/css/app.css", "resources/js/main.js"],
             refresh: true,
         }),
+        vue(),
     ],
 });
